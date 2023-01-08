705f3bed8b1cc9b060b70e07afd64a537093c765
Sebastian Hulse <seb.hulse@yahoo.co.uk>
2023-01-08 22:50:27 +0000

    add Date package, update schema and add components to Plan Modal

--- src/components/dashboard/CreatePlanModal.tsx
+++ src/components/dashboard/CreatePlanModal.tsx
@@ -1,4 +1,12 @@
-import { Modal, Button, Group } from "@mantine/core";
+import {
+  Modal,
+  Button,
+  TextInput,
+  NativeSelect,
+  Switch,
+  Group,
+} from "@mantine/core";
+import { DatePicker } from "@mantine/dates";
 
 type Props = {
   isCreatePlanModalOpen: boolean;
@@ -12,9 +20,34 @@ const CreatePlanModal = (props: Props): JSX.Element => {
       <Modal
         opened={isCreatePlanModalOpen}
         onClose={() => setIsCreatePlanModalOpen(false)}
-        title="Create Plan"
+        title="Plan"
       >
-        {/* Modal content */}
+        <TextInput
+          label="Name"
+          inputWrapperOrder={["label", "input", "error"]}
+          error="Please enter a Workout Plan Name"
+          withAsterisk
+        />
+        <TextInput
+          mt="md"
+          label="Description"
+          inputWrapperOrder={["label", "input"]}
+        />
+        <DatePicker
+          mt="md"
+          placeholder="Pick date"
+          label="Start date"
+          withAsterisk
+        />
+        <NativeSelect
+          mt="md"
+          data={["Weeks", "Months"]}
+          label="Plan interval"
+        />
+        <Switch mt="md" label="Allow user to edit Plan" />
+        <Group position="center">
+          <Button mt="md">Submit</Button>
+        </Group>
       </Modal>
     </>
   );
