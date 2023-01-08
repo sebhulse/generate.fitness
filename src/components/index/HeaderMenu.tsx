4d6530516139cc999a520f9c1184fb02cd699611
Sebastian Hulse <seb.hulse+github@pm.me>
2023-01-08 14:31:33 +0000

    add mantine ui library and build out basic dashboard functionality

---
+++ src/components/index/HeaderMenu.tsx
@@ -0,0 +1,152 @@
+import {
+  createStyles,
+  Header,
+  Group,
+  Button,
+  Divider,
+  Box,
+  Burger,
+  Drawer,
+  ScrollArea,
+} from "@mantine/core";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
+import { useDisclosure } from "@mantine/hooks";
+
+const useStyles = createStyles((theme) => ({
+  link: {
+    display: "flex",
+    alignItems: "center",
+    height: "100%",
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+    textDecoration: "none",
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    fontWeight: 500,
+    fontSize: theme.fontSizes.sm,
+
+    [theme.fn.smallerThan("sm")]: {
+      height: 42,
+      display: "flex",
+      alignItems: "center",
+      width: "100%",
+    },
+
+    ...theme.fn.hover({
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+    }),
+  },
+
+  hiddenMobile: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  hiddenDesktop: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+}));
+
+const HeaderMenu = () => {
+  const { data: sessionData } = useSession();
+
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
+  const { classes, theme } = useStyles();
+
+  return (
+    <Box>
+      <Header height={60} px="md">
+        <Group position="apart" sx={{ height: "100%" }}>
+          <Group
+            sx={{ height: "100%" }}
+            spacing={0}
+            className={classes.hiddenMobile}
+          >
+            <Link href="/" className={classes.link}>
+              Home
+            </Link>
+            <Link href="/dashboard" className={classes.link}>
+              Dashboard
+            </Link>
+          </Group>
+
+          <Group className={classes.hiddenMobile}>
+            <Button
+              onClick={
+                sessionData
+                  ? () => signOut()
+                  : () =>
+                      signIn(undefined, {
+                        callbackUrl: "/dashboard",
+                      })
+              }
+            >
+              {sessionData ? "Sign out" : "Sign in"}
+            </Button>
+            {/* <Button variant="filled">Sign up</Button> */}
+          </Group>
+
+          <Burger
+            opened={drawerOpened}
+            onClick={toggleDrawer}
+            className={classes.hiddenDesktop}
+          />
+        </Group>
+      </Header>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Navigation"
+        className={classes.hiddenDesktop}
+        zIndex={1000000}
+      >
+        <ScrollArea sx={{ height: "calc(100vh - 60px)" }} mx="-md">
+          <Divider
+            my="sm"
+            color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
+          />
+
+          <Link href="/" className={classes.link}>
+            Home
+          </Link>
+          <Link href="/dashboard" className={classes.link}>
+            Dashboard
+          </Link>
+
+          <Divider
+            my="sm"
+            color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
+          />
+
+          <Group position="center" grow pb="xl" px="md">
+            <Button
+              onClick={
+                sessionData
+                  ? () => signOut()
+                  : () =>
+                      signIn(undefined, {
+                        callbackUrl: "/dashboard",
+                      })
+              }
+            >
+              {sessionData ? "Sign out" : "Sign in"}
+            </Button>
+          </Group>
+        </ScrollArea>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default HeaderMenu;
---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+
+export const userRouter = createTRPCRouter({
+  me: protectedProcedure.query(async ({ ctx }) => {
+    const user = await ctx.prisma.user.findUnique({
+      where: {
+        id: ctx.session.user.id,
+      },
+    });
+    return user;
+  }),
+});
