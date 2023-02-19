97f782b0ad03850d0bbadb182f1f929202d0bc7b
Sebastian Hulse <seb.hulse@yahoo.co.uk>
2023-02-19 00:11:39 +0000

    add coming soon page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
+import React, { useState } from "react";
+import { Button, Center, Stack, Title, Text } from "@mantine/core";
 import { type NextPage } from "next";
 import Head from "next/head";
-import HeaderMenu from "../components/index/HeaderMenu";
 
 const Home: NextPage = () => {
+  const [movement, setMovement] = useState("");
+  const [count, setCount] = useState(0);
+  const movements = ["Push up", "Squat", "Sit up"];
+  const handleGenerate = () => {
+    const randomMovement =
+      movements[Math.floor(Math.random() * movements.length)];
+    setMovement(randomMovement ? randomMovement : "Push up");
+    setCount(count + 1);
+  };
+
   return (
     <>
       <Head>
@@ -10,7 +21,31 @@ const Home: NextPage = () => {
         <meta name="description" content="Fitness planning app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HeaderMenu />
+      <Center style={{ width: "100vw", height: "100vh" }}>
+        <Stack align="center" p="md">
+          <Title variant="gradient" gradient={{ from: "indigo", to: "cyan" }}>
+            generate.fitness
+          </Title>
+          <Text align="center" size="lg">
+            <i>Coming soon 🚀</i>
+          </Text>
+          <Text align="center">
+            <a href="https://sebhulse.medium.com/subscribe">Subscribe</a> to be
+            among the first to be notified about the release.
+          </Text>
+          <Button
+            size="lg"
+            m="xl"
+            variant="gradient"
+            gradient={{ from: "indigo", to: "cyan" }}
+            onClick={handleGenerate}
+          >
+            Generate
+          </Button>
+          {movement ? <Text>Suggestion {count}:</Text> : null}
+          {movement ? <Text>{movement}</Text> : null}
+        </Stack>
+      </Center>
     </>
   );
 };
