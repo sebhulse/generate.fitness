0a3e2079145d1b5f0898fecc2e7a28904f033002
Sebastian Hulse <sebastian.hulse@rippleenergy.com>
2023-09-16 17:00:52 +0100

    update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { Button, Center, Text } from "@mantine/core";
+import { Button, Card, Center, Group, Text } from "@mantine/core";
 import { IconPencil, IconPlus, IconPlayerPlay } from "@tabler/icons";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import PublicLayout from "../layouts/PublicLayout";
+import HomeBulletPoints from "../components/index/HomeBulletPoints";
+import HomeOpenSource from "../components/index/HomeOpenSource";
 
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
@@ -74,106 +76,28 @@ const Home: NextPage = () => {
             Start for free
           </Button>
         </Center>
-        <Center>
+        <Center style={{ marginBottom: "4rem" }}>
           <div style={{ maxWidth: "1000px" }}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "start",
-                alignItems: "center",
-                margin: "2rem",
-              }}
-            >
-              <div style={{ height: "50px", width: "50px" }}>
-                <IconPencil size={50} style={{ marginRight: "2rem" }} />
-              </div>
-              <div style={{ marginLeft: "2rem" }}>
-                <Text ta="left" fz="xl" fw={700} style={{ fontSize: "1.5rem" }}>
-                  Customise training plans
-                </Text>
-                <Text fz="md">
-                  With our vast library of exercises and workouts, you can mix
-                  and match to create the perfect plan for you.
-                </Text>
-              </div>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "start",
-                alignItems: "center",
-                margin: "2rem",
-              }}
-            >
-              <div style={{ height: "50px", width: "50px" }}>
-                <IconPlus size={50} style={{ marginRight: "2rem" }} />
-              </div>
-              <div style={{ marginLeft: "2rem" }}>
-                <Text ta="left" fz="xl" fw={700} style={{ fontSize: "1.5rem" }}>
-                  Generate varied workouts
-                </Text>
-                <Text fz="md">
-                  Our intuitive interface and powerful algorithms make it easy
-                  for you to generate personalised workouts to meet your unique
-                  fitness goals.
-                </Text>
-              </div>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "start",
-                alignItems: "center",
-                margin: "2rem",
-              }}
-            >
-              <div style={{ height: "50px", width: "50px" }}>
-                <IconPlayerPlay size={50} style={{ marginRight: "2rem" }} />
-              </div>
-              <div style={{ marginLeft: "2rem" }}>
-                <Text ta="left" fz="xl" fw={700} style={{ fontSize: "1.5rem" }}>
-                  Smash your workouts in real-time
-                </Text>
-                <Text fz="md">
-                  Experience the future of fitness with your real-time dynamic
-                  guide, ensuring you stay on track and motivated throughout
-                  your session.
-                </Text>
-              </div>
-            </div>
-          </div>{" "}
-        </Center>
-        <Center>
-          <div>
-            <Text>Embrace Open Source Fitness</Text>
-            <Text>
-              At generate.fitness, we believe in the power of collaboration.
-              That's why we've chosen to open source our platform. It's about
-              transparency, flexibility, and community.
-            </Text>
-            <Text>What does this mean for you?</Text>
-            <Text>
-              <ul>
-                <li>
-                  Full Control: Customize your fitness experience according to
-                  your unique preferences.
-                </li>
-                <li>
-                  Collective Innovation: Join hands with fellow enthusiasts to
-                  drive fitness technology forward.
-                </li>
-                <li>
-                  Clear Transparency: Gain insights into how your fitness data
-                  is handled and make informed choices.
-                </li>
-                <li>
-                  Endless Opportunities: Explore, experiment, and create new
-                  possibilities.
-                </li>
-              </ul>
-              Together, we're redefining fitness technology for a more open and
-              honest future.
-            </Text>
+            <HomeBulletPoints />
+            <HomeOpenSource />
+            <Center>
+              <Button
+                type="submit"
+                mt="xl"
+                size="lg"
+                radius="xl"
+                variant="gradient"
+                gradient={{ from: "indigo", to: "cyan" }}
+                onClick={() => {
+                  sessionData?.user
+                    ? router.push("/dashboard")
+                    : signIn(undefined, { callbackUrl: callbackUrl });
+                }}
+                style={{ marginBottom: "4rem" }}
+              >
+                Start for free
+              </Button>
+            </Center>
           </div>
         </Center>
       </PublicLayout>
