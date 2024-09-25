50edfe09bcf679413210219d311e96de86c260bc
Sebastian Hulse <sebastian.hulse@rippleenergy.com>
2024-09-25 21:27:49 +0100

    baseline migrations

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,298 @@
+yarn run v1.22.17
+$ /Users/Seb/Documents/GitHub/generate.fitness/node_modules/.bin/prisma migrate diff --from-empty --to-schema-datamodel prisma/schema.prisma --script
+-- CreateTable
+CREATE TABLE `Account` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `provider` VARCHAR(191) NOT NULL,
+    `providerAccountId` VARCHAR(191) NOT NULL,
+    `refresh_token` TEXT NULL,
+    `access_token` TEXT NULL,
+    `expires_at` INTEGER NULL,
+    `token_type` VARCHAR(191) NULL,
+    `scope` VARCHAR(191) NULL,
+    `id_token` TEXT NULL,
+    `session_state` VARCHAR(191) NULL,
+
+    INDEX `Account_userId_idx`(`userId`),
+    UNIQUE INDEX `Account_provider_providerAccountId_key`(`provider`, `providerAccountId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `sessionToken` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Session_sessionToken_key`(`sessionToken`),
+    INDEX `Session_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `VerificationToken` (
+    `identifier` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `VerificationToken_token_key`(`token`),
+    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `emailVerified` DATETIME(3) NULL,
+    `image` VARCHAR(191) NULL,
+    `role` ENUM('PERSONAL', 'TRAINER', 'ADMIN') NOT NULL DEFAULT 'PERSONAL',
+    `trainerId` VARCHAR(191) NULL,
+    `goals` VARCHAR(191) NULL,
+    `notes` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    INDEX `User_trainerId_idx`(`trainerId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `UserTag` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `UserTag_name_key`(`name`),
+    INDEX `UserTag_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Plan` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `startDate` DATETIME(3) NULL,
+    `planInterval` ENUM('Weeks', 'Months') NOT NULL DEFAULT 'Weeks',
+    `allowDisplayUserEdit` BOOLEAN NOT NULL DEFAULT false,
+
+    INDEX `Plan_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `PlanTag` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NULL,
+    `planId` VARCHAR(191) NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `PlanTag_name_key`(`name`),
+    INDEX `PlanTag_planId_idx`(`planId`),
+    INDEX `PlanTag_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `PlanSection` (
+    `id` VARCHAR(191) NOT NULL,
+    `planId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `order` INTEGER NOT NULL,
+
+    INDEX `PlanSection_planId_idx`(`planId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Workout` (
+    `id` VARCHAR(191) NOT NULL,
+    `planSectionId` VARCHAR(191) NULL,
+    `userId` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `order` INTEGER NOT NULL DEFAULT 0,
+    `duration` INTEGER NULL,
+    `usesEquipment` BOOLEAN NOT NULL DEFAULT false,
+    `workoutTypeId` VARCHAR(191) NOT NULL,
+    `workoutTargetAreaId` VARCHAR(191) NOT NULL,
+    `workoutIntensityId` VARCHAR(191) NOT NULL,
+    `allowDisplayUserEdit` BOOLEAN NOT NULL DEFAULT false,
+    `isDone` BOOLEAN NOT NULL DEFAULT false,
+
+    INDEX `Workout_userId_idx`(`userId`),
+    INDEX `Workout_planSectionId_idx`(`planSectionId`),
+    INDEX `Workout_workoutTypeId_idx`(`workoutTypeId`),
+    INDEX `Workout_workoutTargetAreaId_idx`(`workoutTargetAreaId`),
+    INDEX `Workout_workoutIntensityId_idx`(`workoutIntensityId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `WorkoutTag` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NULL,
+    `workoutId` VARCHAR(191) NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `WorkoutTag_name_key`(`name`),
+    INDEX `WorkoutTag_workoutId_idx`(`workoutId`),
+    INDEX `WorkoutTag_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `WorkoutType` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `WorkoutType_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `WorkoutTargetArea` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `WorkoutTargetArea_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `WorkoutIntensity` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `WorkoutIntensity_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `WorkoutSection` (
+    `id` VARCHAR(191) NOT NULL,
+    `workoutId` VARCHAR(191) NOT NULL,
+    `workoutSectionTypeId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `order` INTEGER NOT NULL,
+
+    INDEX `WorkoutSection_workoutId_idx`(`workoutId`),
+    INDEX `WorkoutSection_workoutSectionTypeId_idx`(`workoutSectionTypeId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Exercise` (
+    `id` VARCHAR(191) NOT NULL,
+    `workoutSectionId` VARCHAR(191) NOT NULL,
+    `movementId` VARCHAR(191) NOT NULL,
+    `duration` INTEGER NOT NULL,
+    `order` INTEGER NOT NULL,
+    `durationInterval` ENUM('Seconds', 'Metres', 'Reps') NOT NULL DEFAULT 'Seconds',
+    `rest` INTEGER NOT NULL DEFAULT 0,
+
+    INDEX `Exercise_workoutSectionId_idx`(`workoutSectionId`),
+    INDEX `Exercise_movementId_idx`(`movementId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Movement` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `combinationMovement` BOOLEAN NOT NULL DEFAULT false,
+    `equipmentId` VARCHAR(191) NULL,
+    `equipmentIntensity` ENUM('Lightest', 'Light', 'Medium', 'Heavy', 'Heaviest') NULL,
+
+    UNIQUE INDEX `Movement_name_key`(`name`),
+    INDEX `Movement_equipmentId_idx`(`equipmentId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `WorkoutSectionType` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `WorkoutSectionType_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Equipment` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Equipment_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_UserToUserTag` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_UserToUserTag_AB_unique`(`A`, `B`),
+    INDEX `_UserToUserTag_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_DisplayUsers` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_DisplayUsers_AB_unique`(`A`, `B`),
+    INDEX `_DisplayUsers_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_MovementToWorkoutType` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_MovementToWorkoutType_AB_unique`(`A`, `B`),
+    INDEX `_MovementToWorkoutType_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_MovementToWorkoutTargetArea` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_MovementToWorkoutTargetArea_AB_unique`(`A`, `B`),
+    INDEX `_MovementToWorkoutTargetArea_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_MovementToWorkoutIntensity` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_MovementToWorkoutIntensity_AB_unique`(`A`, `B`),
+    INDEX `_MovementToWorkoutIntensity_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_MovementToWorkoutSectionType` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_MovementToWorkoutSectionType_AB_unique`(`A`, `B`),
+    INDEX `_MovementToWorkoutSectionType_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+Done in 0.46s.
